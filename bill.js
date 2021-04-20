2af57efbd752bd888c49a73ed2ed775b4ad9b234
Muzain187 <60264379+Muzain187@users.noreply.github.com>
2021-04-20 23:41:03 +0530

    Add files via upload

---
+++ bill.js
@@ -0,0 +1,143 @@
+console.log("Bill");
+let AddBtn = document.getElementById('AddBtn');
+let TotalAmnt = document.getElementById('TotalAmnt');
+let CustName = document.getElementById('CustName');
+console.log('Customer Name ',CustName.value);
+localStorage.setItem("CustomerName",CustName.value);
+CustName.value='';
+
+let Total = 0;
+showlist();
+
+/* *******************************FUNCTION TO ADD LIST TO LOCAL STORAGE****************************  */
+AddBtn.addEventListener('click',function(){
+    console.log("created");
+    // let addTxt = document.getElementById('addTxt');
+    let ItemName = document.getElementById('ItemName');
+    let ItemQuantity = document.getElementById('Quantity');
+    let Itemprice = document.getElementById('price');
+
+    // let notes = localStorage.getItem("notes");
+    let Name = localStorage.getItem("Name");
+    let Quantity = localStorage.getItem("Quantity");
+    let Price = localStorage.getItem("Price");
+    if(Name == null && Quantity==null && Price==null){
+        // notesObj = [];
+        NameObj = [];
+        QuantityObj = [];
+        PriceObj = [];
+    }
+    else{
+        // notesObj = JSON.parse(notes);
+        NameObj = JSON.parse(Name);
+        QuantityObj = JSON.parse(Quantity);
+        PriceObj = JSON.parse(Price);
+    }
+    // notesObj.push(addTxt.value);
+    NameObj.push(ItemName.value);
+    QuantityObj.push(ItemQuantity.value);
+    PriceObj.push(Itemprice.value * ItemQuantity.value);
+
+    // localStorage.setItem("notes",JSON.stringify(notesObj));
+    localStorage.setItem("Name",JSON.stringify(NameObj));
+    localStorage.setItem("Quantity",JSON.stringify(QuantityObj));
+    localStorage.setItem("Price",JSON.stringify(PriceObj));
+
+    // addTxt.value = '';
+    ItemName.value = '';
+    ItemQuantity.value = '';
+    Itemprice.value = '';
+
+    // console.log(notesObj);
+    console.log(NameObj);
+    console.log(QuantityObj);
+    console.log(PriceObj);
+
+    // console.log(NameObj.length);
+    Total += parseFloat(PriceObj[i]);
+    showlist();
+    
+})
+/* ********************************END OF INSERTING TO LOCAL STORAGE****************************************** */ 
+
+//*****************function to show the list of items********************** 
+function showlist(){
+    console.log("list showing here");
+    let Name = localStorage.getItem("Name");
+    let Quantity = localStorage.getItem("Quantity");
+    let Price = localStorage.getItem("Price");
+    if(Name == null && Quantity==null && Price==null){
+        // notesObj = [];
+        NameObj = [];
+        QuantityObj = [];
+        PriceObj = [];
+    }
+    else{
+        // notesObj = JSON.parse(notes);
+        NameObj = JSON.parse(Name);
+        QuantityObj = JSON.parse(Quantity);
+        PriceObj = JSON.parse(Price);
+    }
+    let html ="";
+    for(i=0;i<NameObj.length;i++){
+        p = PriceObj[i]*(1/QuantityObj[i]);
+        // Total += Number(PriceObj[i]);
+        console.log("Display ",Total,typeof(Total));
+        html +=`
+                  <tr>
+                    <th scope="row">${i+1}</th>
+                    <td scope="row" >${NameObj[i]}</td>
+                    <td scope="row" >${QuantityObj[i]}X${p}</td>
+                    <td scope="row" >${PriceObj[i]}</td>
+                    <td >
+                    <input type="image" src="deleteimage.png" name="delete" width="50" height="50" alt="delete" id="${i}" style="float:right;margin-right: 6em;"onclick="deleteList(this.id)"></td> 
+                  </tr>
+                  
+                `;
+        TotalAmnt.innerHTML = `${Total}`;
+    }
+    let total = parseFloat(Total);
+    localStorage.setItem("TotalAmount",total.toString());
+    let listElm = document.getElementById('list');
+    console.log(listElm);
+    listElm.innerHTML = html;
+}
+//******************************************END of Show list***********************************
+
+//**************************Function to Delete list************************
+function deleteList(index){
+    // let notes = localStorage.getItem("notes");
+    let Name = localStorage.getItem("Name");
+    let Quantity = localStorage.getItem("Quantity");
+    let Price = localStorage.getItem("Price");
+    if(Name == null && Quantity==null && Price==null){
+        // notesObj = [];
+        NameObj = [];
+        QuantityObj = [];
+        PriceObj = [];
+    }
+    else{
+        // notesObj = JSON.parse(notes);
+        NameObj = JSON.parse(Name);
+        QuantityObj = JSON.parse(Quantity);
+        PriceObj = JSON.parse(Price);
+    }
+    // notesObj.splice(index,1);
+    NameObj.splice(index,1);
+    QuantityObj.splice(index,1);
+    let presentAmnt = PriceObj[index];//storing the amount in temporary memory
+    PriceObj.splice(index,1);
+
+    // localStorage.setItem("notes",JSON.stringify(notesObj));
+    localStorage.setItem("Name",JSON.stringify(NameObj));
+    localStorage.setItem("Quantity",JSON.stringify(QuantityObj));
+    localStorage.setItem("Price",JSON.stringify(PriceObj));
+
+    console.log("Deleted");
+    console.log(NameObj.length);
+    Total -= parseFloat(presentAmnt);
+    console.log("Deletion ",Total,typeof(Total));
+    showlist();
+    
+}
+/* ***************************************END OF DELETION***************************** */
---
+++ final.js
@@ -0,0 +1,72 @@
+let CName = localStorage.getItem("CmpName");
+let CAddr = localStorage.getItem("CmpAddr");
+console.log("cmp addr",CAddr);
+let CEmail = localStorage.getItem("CmpEmail");
+let CPhno = localStorage.getItem("CmpPhno");
+
+let CmpName = document.getElementById('CmpName');
+let CmpAddr = document.getElementById('CmpAddress');
+let CmpContact = document.getElementById('CmpContact');
+
+let TotalAmnt = document.getElementById('TotalAmnt');
+
+console.log("hiii from final Bill");
+
+let Name = localStorage.getItem('Name');
+let Quantity = localStorage.getItem('Quantity');
+let Price = localStorage.getItem('Price');
+console.log(typeof(Name));
+
+NameObj = JSON.parse(Name);
+QuantityObj = JSON.parse(Quantity);
+PriceObj = JSON.parse(Price);
+
+console.log(typeof(NameObj));
+// console.log(NameObj[0]);
+
+/* ***************Display Company Name Address And Contact *********************** */
+
+CmpName.innerHTML = CName;
+CmpAddr.innerHTML = `<i class="fa fa-map-marker"></i> `+CAddr;
+CmpContact.innerHTML = `<i class="fa fa-phone"></i>
+`+CPhno+`<br>`+`<i class="fa fa-envelope"></i>
+`+CEmail;
+
+/* ***************End Of Display********************************* */
+
+/* ******************display the items*************************** */
+let html = "";
+for(i=0;i<NameObj.length;i++){
+    p = PriceObj[i]*(1/QuantityObj[i]);
+    html +=`
+    <tr>
+      <th scope="row" style="width:15em">${i+1}</th>
+      <td scope="row" style="width:20em">${NameObj[i]}</td>
+      <td scope="row" style="width:20em">${QuantityObj[i]}X${p}</td>
+      <td scope="row" style="width:20em">${PriceObj[i]}</td>
+    </tr>   
+  `;
+}
+let listElm = document.getElementById('list');
+console.log(listElm);
+listElm.innerHTML = html;
+/* **************End of Display******************************* */
+
+/* ******************Customer Name and Total Amount**** */
+let CustName = document.getElementById('CustName');
+let CustomerName = localStorage.getItem('CustomerName');
+console.log('Customer name',CustomerName);
+CustName.innerHTML = `To:<i class="fa fa-user"></i> ${CustomerName}`;
+
+let TotalAmount = localStorage.getItem('TotalAmount');
+TotalAmnt.innerHTML = ` <strong>${TotalAmount}</strong>`;
+console.log('Total Amount',TotalAmount);
+/* *****End*** */
+
+/* *****Convert Html to pdf *********************** */
+function Download(){
+  let html = document.getElementById('head');
+  html2pdf()
+  .from(html)
+  .save();
+}
